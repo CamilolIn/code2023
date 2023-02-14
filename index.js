d59f6298bda0089ce6321123ca79da721de73f96
Nelson-Lindarte01_lmig <114938845+CamiloLindarte@users.noreply.github.com>
2023-02-14 15:35:41 -0500

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("HOLA MUNDO")
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 4002;
+
+app.get("/", (req, res) => {
+  res.send("Hola mundo");
+});
+
+app.get("/all", (req, res) => {
+  res.send("Hola mundo ALL, todos los usuarios");
+});
+
+app.get("/all-2", (req, res) => {
+    res.send("ULTIMO CAMBIO !!!");
+  });
+  
+
+app.listen(PORT, () => {
+  console.log("Server ok!");
+});
